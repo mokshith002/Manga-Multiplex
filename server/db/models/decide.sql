6e442300f73454575bd1a45fe25e67f3564e178b
an-bhv <f20201892@hyderabad.bits-pilani.ac.in>
2022-04-25 16:14:33 +0530

    emplyeeid->empId

--- server/db/models/decide.sql
+++ server/db/models/decide.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS decide;
 
 CREATE TABLE decide (
-    employeeId INTEGER REFERENCES employee (employeeId),
+    employeeId INTEGER REFERENCES employee (empId),
     showId INTEGER REFERENCES show (showId)
     
 );
