3af826f2d8e38d52f5b7a99fef64c68dc1bcfccc
an-bhv <f20201892@hyderabad.bits-pilani.ac.in>
2022-04-25 16:05:49 +0530

    employeeid -> empId

--- server/db/models/ticket.sql
+++ server/db/models/ticket.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS ticket;
 CREATE TABLE ticket (
     ticketId SERIAL PRIMARY KEY,
     showId INTEGER REFERENCES show (showId),
-    employeeId INTEGER REFERENCES employee (employeeId),
+    employeeId INTEGER REFERENCES employee (empId),
     seatNo VARCHAR(10),
     price INTEGER
 );
