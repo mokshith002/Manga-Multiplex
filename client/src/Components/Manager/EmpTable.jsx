de59b05bd77160adbfa2b4262fd453bd6a26aba3
mokshith002 <mokshith002@gmail.com>
2022-04-27 23:54:33 +0530

    final changes

--- client/src/Components/Manager/EmpTable.jsx
+++ client/src/Components/Manager/EmpTable.jsx
@@ -3,47 +3,66 @@ import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
 import DeleteIcon from '@mui/icons-material/Delete';
 import FileCopyIcon from '@mui/icons-material/FileCopy';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import axios from "axios";
+import "./R.css";
+// const initialRows = [
+//   {
+//     id: 1,
+//     name: 'Damien',
+//     email:'1234@gmail.com',
+//     role:'counter staff'
+//   },
+//   {
+//     id: 2,
+//     name: 'Damien',
+//     email:'1234@gmail.com',
+//     role:'counter staff'
+//   },
+//   {
+//     id: 3,
+//     name: 'Damien',
+//     email:'1234@gmail.com',
+//     role:'counter staff'
+//   },
+//   {
+//     id: 4,
+//     name: 'Damien',
+//     email:'1234@gmail.com',
+//     role:'counter staff'
+//   },
+//   {
+//     id: 5,
+//     name: 'Damien',
+//     email:'1234@gmail.com',
+//     role:'counter staff'
+//   },
+// ];
 
-const initialRows = [
-  {
-    id: 1,
-    name: 'Damien',
-    email:'1234@gmail.com',
-    role:'counter staff'
-  },
-  {
-    id: 2,
-    name: 'Damien',
-    email:'1234@gmail.com',
-    role:'counter staff'
-  },
-  {
-    id: 3,
-    name: 'Damien',
-    email:'1234@gmail.com',
-    role:'counter staff'
-  },
-  {
-    id: 4,
-    name: 'Damien',
-    email:'1234@gmail.com',
-    role:'counter staff'
-  },
-  {
-    id: 5,
-    name: 'Damien',
-    email:'1234@gmail.com',
-    role:'counter staff'
-  },
-];
+
 
 export default function Grid() {
-  const [rows, setRows] = React.useState(initialRows);
+
+  const [emp, setEmp] = React.useState([]);
+
+const URL = `http://localhost:${5000}`;
+
+const getEmps = async () => {
+  const res = await axios.get(`${URL}/movie`);
+  setEmp(res.data.map(_emp => ({
+    id: _emp.movieid,
+    name: _emp.moviename
+  })))
+}
+
+React.useEffect(() => {
+  getEmps();
+})
+  // const [rows, setRows] = React.useState(emp);
 
   const deleteUser = React.useCallback(
     (id) => () => {
       setTimeout(() => {
-        setRows((prevRows) => prevRows.filter((row) => row.id !== id));
+        setEmp((prevEmp) => prevEmp.filter((row) => row.id !== id));
       });
     },
     [],
@@ -51,7 +70,7 @@ export default function Grid() {
   const viewEmp = React.useCallback(
     (id) => () => {
       setTimeout(() => {
-        setRows((prevRows) => prevRows.filter((row) => row.id !== id));
+        setEmp((prevEmp) => prevEmp.filter((row) => row.id !== id));
       });
     },
     [],
@@ -59,10 +78,9 @@ export default function Grid() {
 
   const columns = React.useMemo(
     () => [
-      { field: 'id', type: 'string', headerAlign: 'center', align: 'center', flex:1 },
-      { field: 'name', type: 'string', headerAlign: 'center', align: 'center', flex:2},
-      {field: 'email',type:'string',width:200, headerAlign: 'center', align: 'center', flex:2},
-      {field: 'role',type:'string', headerAlign: 'center', align: 'center', flex: 2},
+      { field: 'id', headerName: 'Movie ID', type: 'string', headerAlign: 'center', align: 'center', flex:1 },
+      { field: 'name',  headerName: 'Movie Name', type: 'string', headerAlign: 'center', align: 'center', flex:2},
+      
       {
         field: 'del',
         headerName: 'Delete',headerAlign: 'center', align: 'center',
@@ -96,13 +114,13 @@ export default function Grid() {
   );
 
   return (
-    <>
-      <div className="row text-center">
-        <h2 className="mb-5 _heading">All Employees</h2>
+    <div className="__bg">
+      <div className="row text-center __bg">
+        <h2 className="mb-5 _heading ">All Movies</h2>
       </div>
-      <div style={{ height: 550, width: "60%", margin: "auto" }}>
-        <DataGrid columns={columns} rows={rows} />
+      <div className="" style={{ height: 550, width: "60%", margin: "auto" }}>
+        <DataGrid columns={columns} rows={emp} />
       </div>
-    </>
+    </div>
   );
 }
--- server/db/models/decide.sql
+++ server/db/models/decide.sql
@@ -7,13 +7,13 @@ CREATE TABLE decide (
 
 
 
-INSERT INTO decide	VALUES(	1	,	1	);
-INSERT INTO decide	VALUES(	1	,	2	);
-INSERT INTO decide	VALUES(	1	,	3	);
-INSERT INTO decide	VALUES(	1	,	4	);
-INSERT INTO decide	VALUES(	1	,	5	);
-INSERT INTO decide	VALUES(	2	,	6	);
-INSERT INTO decide	VALUES(	2	,	7	);
-INSERT INTO decide	VALUES(	2	,	8	);
-INSERT INTO decide	VALUES(	2	,	9	);
-INSERT INTO decide	VALUES(	2	,	10	);
+INSERT INTO decide VALUES(1, 1);
+INSERT INTO decide VALUES(1, 2);
+INSERT INTO decide VALUES(1, 3);
+INSERT INTO decide VALUES(1, 4);
+INSERT INTO decide VALUES(1, 5);
+INSERT INTO decide VALUES(2, 6);
+INSERT INTO decide VALUES(2, 7);
+INSERT INTO decide VALUES(2, 8);
+INSERT INTO decide VALUES(2, 9);
+INSERT INTO decide VALUES(2, 10);
