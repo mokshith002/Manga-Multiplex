6fffcfeb44a7ed27a2ec229e96b272c9a28af5d1
an-bhv <80155345+an-bhv@users.noreply.github.com>
2022-04-27 22:52:43 +0530

    Update show.sql

--- server/db/models/show.sql
+++ server/db/models/show.sql
@@ -8,4 +8,13 @@ CREATE TABLE show (
     price INTEGER
 );
 
-INSERT INTO show (hallNo, movieId, startTime, price) VALUES (1, 1, '10:00:00', 250);
+INSERT INTO SHOW (	hallNo,movieId,startTime,price	)VALUES(	1	,	1	,	11:00:00	,	150	)	;
+INSERT INTO SHOW (	hallNo,movieId,startTime,price	)VALUES(	2	,	2	,	12:00:00	,	150	)	;
+INSERT INTO SHOW (	hallNo,movieId,startTime,price	)VALUES(	3	,	3	,	13:00:00	,	150	)	;
+INSERT INTO SHOW (	hallNo,movieId,startTime,price	)VALUES(	4	,	4	,	14:00:00	,	150	)	;
+INSERT INTO SHOW (	hallNo,movieId,startTime,price	)VALUES(	5	,	5	,	15:00:00	,	150	)	;
+INSERT INTO SHOW (	hallNo,movieId,startTime,price	)VALUES(	6	,	6	,	16:00:00	,	150	)	;
+INSERT INTO SHOW (	hallNo,movieId,startTime,price	)VALUES(	7	,	7	,	17:00:00	,	150	)	;
+INSERT INTO SHOW (	hallNo,movieId,startTime,price	)VALUES(	8	,	8	,	18:00:00	,	150	)	;
+INSERT INTO SHOW (	hallNo,movieId,startTime,price	)VALUES(	9	,	9	,	19:00:00	,	150	)	;
+INSERT INTO SHOW (	hallNo,movieId,startTime,price	)VALUES(	10	,	10	,	20:00:00	,	150	)	;
