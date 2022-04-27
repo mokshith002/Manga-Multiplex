813ad52ec0079e48d2f61b81a7f918cefe8a3002
an-bhv <80155345+an-bhv@users.noreply.github.com>
2022-04-27 23:06:59 +0530

    Update decide.sql

--- server/db/models/decide.sql
+++ server/db/models/decide.sql
@@ -5,3 +5,15 @@ CREATE TABLE decide (
     showId INTEGER REFERENCES show (showId)
 );
 
+
+
+INSERT INTO decide	VALUES(	1	,	1	);
+INSERT INTO decide	VALUES(	1	,	2	);
+INSERT INTO decide	VALUES(	1	,	3	);
+INSERT INTO decide	VALUES(	1	,	4	);
+INSERT INTO decide	VALUES(	1	,	5	);
+INSERT INTO decide	VALUES(	2	,	6	);
+INSERT INTO decide	VALUES(	2	,	7	);
+INSERT INTO decide	VALUES(	2	,	8	);
+INSERT INTO decide	VALUES(	2	,	9	);
+INSERT INTO decide	VALUES(	2	,	10	);
